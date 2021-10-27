c02f3e92a4607c42e6a9a7a221a5bce000e3794d
MatiasAGomezJ <matiasgomez@cifpfbmoll.eu>
2021-10-27 19:06:17 +0200

    Comp

---
+++ comp/comp2.py
@@ -0,0 +1,13 @@
+def comp(array1, array2):
+
+    if array2[i] != array1[0]**2:
+        print('hola')
+
+if __name__ == '__main__':
+    a1 = [2,3]
+    a2 = [4, 9]
+    assert comp(a1,a2) == True
+
+    
+    
+     

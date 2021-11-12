fdd5ad9f52360cb15d32f08d89e090cda40e6fd4
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2021-11-12 15:14:58 +0100

    LZ78

--- LZ78_compresion/LZ78_compresion.py
+++ LZ78_compresion/LZ78_compresion.py
@@ -1,24 +1,42 @@
-def encoder(data):
-    saved_string = { 0 : ''}
-    
-    i = 0
-    position_saved = 0
-    solution = ""
-    while i < len(data):
-        acumulador = ''
-        while acumulador in saved_string.values():
-            acumulador += data[i]
-            i += 1
-            
-        # acumulador += data[1] 
-        solution += str(saved_string.keys()[saved_string.values().index(acumulador[0:-1])) + data[i-1]
-        position_saved += 1
-        print(solution)
-        saved_string[position_saved] = acumulador
+def conversor_dict_to_string(d):
 
-    print("pausa")
-    return solution
+    assert isinstance(d, dict)
+
+    string = ""
+
+    for item in d:
+        string += str(item) + d[item]
+
+    return string
+
+def encoder(data):
+
+    assert isinstance(data, str)
+
+    solucion = ""
+
+    strings = { 0 : ''} 
+
+    contador_palbras_diferentes = 1
+    acumulador = ""
+    for char in data:
+        acumulador += char
+        if acumulador not in strings.values():
+            strings[contador_palbras_diferentes] = acumulador
+            solucion += "0" + str(acumulador[len(acumulador)-1])
+            acumulador = ""
+            contador_palbras_diferentes += 1
+
+    solution = conversor_dict_to_string(strings)
+
+    return solucion
 
 
 if __name__ == '__main__':
-    assert encoder('ABAABABAABAB') == '0A0B1A2A4A4B' 
+    assert encoder('ABAABABAABAB') == '0A0B1A2A4A4B'
+    assert encoder('ABAABABAABAB') == '0A0B1A2A4A4B'
+    assert encoder('ABAABABAABABAA') == '0A0B1A2A4A4B3'
+    assert encoder('ABBCBCABABCAABCAABBCAA') == '0A0B2C3A2A4A6B6'
+    assert encoder('AAAAAAAAAAAAAAA') == '0A1A2A3A4A'
+    assert encoder('ABCABCABCABCABCABC') == '0A0B0C1B3A2C4C7A6'
+    assert encoder('ABCDDEFGABCDEDBBDEAAEDAEDCDABC') == '0A0B0C0D4E0F0G1B3D0E4B2D10A1E4A10D9A2C'

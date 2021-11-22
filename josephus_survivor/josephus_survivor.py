b419264524e82c935d1315ec21480ec3367f0a26
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2021-11-22 04:41:13 +0100

    Josephus acabado y comentado

--- josephus_survivor/josephus_survivor.py
+++ josephus_survivor/josephus_survivor.py
@@ -1,25 +1,33 @@
 def josephus_survivor(cantidad_personas, num_saltos):
-    lista_personas = list(range(1, cantidad_personas + 1))
+
+    lista_personas = list(range(1, cantidad_personas + 1)) # Lista en la cual se van a ir eliminando a las personas
     
-    indice = 0
+    indice = 0 # El indice se utiliza para saber quien va a ser eliminado
 
-    len_lista_personas = len(lista_personas)
-    while len(lista_personas) > 1:  # Pongo el len(..) porque si no tengo que actualizar la variable cada ciclo
+    personas_restantes = len(lista_personas) # Creo una variable con la cantidad de items dentro de lista_personas ya que se utiliza varias veces, al principio sera igual a la cantidad_personas
 
-        indice += num_saltos
+    while personas_restantes > 1: # Mientras haya mas de una persona sigue
 
-        if indice > len_lista_personas:
-            indice -= len_lista_personas
+        indice += num_saltos # Se añade la cantidad de saltos desde el ultimo eliminado, al principio será desde cero
 
-        principio_lista = lista_personas[:indice-1]
-        final_lista = lista_personas[indice:]
-        lista_personas = principio_lista + final_lista
-        indice -= 1
+        # Si el indice esta fuera de la lista no podemos eliminar nada, por lo tanto ...
+        while indice > personas_restantes:  # ... mientras el indice sea mayor a la cantidad de personas restantes ...
+            indice -= personas_restantes    # ... restaremos la cantidad de personas al indice hasta que indice este en el rango
 
+        # Lo que hago para eliminar a alguien es:
+        # 1/3. Creo una lista desde la primera persona hasta la eliminada, sin contarla
+        # 2/3. Creo una lista desde la persona eliminada, sin contarla, hasta la ultima persona
+        # 3/3. Junto las dos listas las cuales ninguna tiene a la persona que hay que eliminar
 
+        principio_lista = lista_personas[:indice-1]     # Creacion desde el principio de la lista hasta el eliminado
+        final_lista = lista_personas[indice:]           # Creacion desde el eliminado hasta el final de la lista
+        lista_personas = principio_lista + final_lista  # Junto las dos listas
 
+        indice -= 1 # Es necesario bajar una posicion porque se tiene que empezar a contar a partir del anterior al eliminado
+        
+        personas_restantes = len(lista_personas) # Actualizo la cantidad de personas restantes
 
-    return
+    return lista_personas[0] # Devuelvo la unica persona restante
 
 if __name__ == '__main__':
     assert josephus_survivor(7, 3) == 4
